bbacf6f808902323522c91a8a1698e2b2afb565a
Mansur1075 <Mansur1075@users.noreply.github.com>
2026-05-23 00:01:25 +0600

    mansurjdgufrheiugi

--- src/Component/CartItem.jsx
+++ src/Component/CartItem.jsx
@@ -9,6 +9,10 @@ const CartItem = ({ img, name, Price, Subtotal, className }) => {
                     <img className='h-[55px] w-[55px]' src={img} alt="" />
                     <img src={x} className={`absolute -top-1 -left-3 ${className}`} alt="" />
 
+
+
+
+
                 </div>
                 <p>{name}</p>
             </div>
